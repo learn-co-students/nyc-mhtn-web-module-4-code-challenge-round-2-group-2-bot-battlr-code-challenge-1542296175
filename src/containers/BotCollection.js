486666beaae2531e48d3f96f872c69161d6fc04a
sinyoungnam <sinnam90@gmail.com>
2018-11-15 12:05:43 -0500

    done

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -3,12 +3,18 @@ import BotCard from "../components/BotCard";
 
 class BotCollection extends React.Component {
   //your code here
+  mapThroughBots = () => {
+    return this.props.bots.map((bot) => {
+      return <BotCard bot={bot} key={bot.id} handleClickBot={this.props.handleClickBot}/>
+    })
+  }
 
   render(){
+    // console.log(this.props)
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
+    		  {this.mapThroughBots()}
     		  Collection of all bots
     		</div>
   	  </div>
--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -3,20 +3,26 @@ import BotCard from "../components/BotCard";
 
 class YourBotArmy extends React.Component {
   //your bot army code here...
+  mapThroughSelectedBots = () => {
+    return this.props.selectedBots.map((bot) => {
+      return <BotCard bot={bot} key={bot.id} handleClickBot={this.props.handleClickBot}/>
+    })
+  }
 
   render(){
+    // console.log(this.props);
     return (
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
-            {/*...and here...*/}
+            {this.mapThroughSelectedBots()}
             Your Bot Army
           </div>
         </div>
       </div>
     );
   }
-  
+
 };
 
 export default YourBotArmy;
